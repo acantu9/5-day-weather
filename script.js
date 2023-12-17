8cc8b5684d7a61f15694a9f93469127068585b68
acantu9 <adrianaacantu@gmail.com>
2023-12-16 21:15:15 -0600

    added Bootstrap CSS and styling to index.html/refractored script.js and added code for 5 day forecast

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Move the API key to a separate configuration file or environment variable
+// API key
 const apiKey = 'c7e81f239bf65cf328d2b68e7626567f';
 
 // Get the form element and input field
@@ -19,53 +19,84 @@ document.addEventListener('DOMContentLoaded', function() {
         const cityName = input.value.trim();
       
         if (cityName) {
-          const geoApiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
+            const geoApiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
       
-          fetch(geoApiUrl)
+            fetch(geoApiUrl)
             .then(response => response.json())
             .then(geoData => {
-              const { lat, lon } = geoData[0];
+                const { lat, lon } = geoData[0];
       
-              const weatherApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+                const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+                const fiveDayWeatherApi = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+                
+            fetch(weatherApiUrl)
+            .then(response => response.json())
+            .then(weatherData => {
+                console.log(weatherData);
+                console.log('weather data');
       
-              fetch(weatherApiUrl)
-                .then(response => response.json())
-                .then(weatherData => {
-                    console.log(weatherData);
-      
-                    // Update the cityData array
-                    cityData.push(weatherData);
+                // Update the cityData array
+                cityData.push(weatherData);
+                    
+                // Retrieve the temperature in Kelvin
+                let kelvinTemp = weatherData.main.temp;
+                    
+                // Convert Kelvin to Fahrenheit
+                let fahrenheitTemp = (kelvinTemp - 273.15) * 9/5 + 32;
+                    
+                // Update HTML elements with weather information
+                document.getElementById('cityName').innerHTML = weatherData.name;
+                document.getElementById('temperature').innerHTML = "Temp: " + fahrenheitTemp.toFixed(2) + " °F";
+                document.getElementById('wind').innerHTML = "Wind Speed: " + weatherData.wind.speed + " mph";
+                document.getElementById('humidity').innerHTML = "Humidity: " + weatherData.main.humidity + " %";
+            });
+            
+            // Fetch the weather data
+            fetch(fiveDayWeatherApi)
+            .then(response => response.json())
+            .then(fiveDayWeatherData => {
+                console.log(fiveDayWeatherData);
+                console.log("five day forecast data");
 
-                    // Assuming the temperature in Kelvin is stored in a variable called kelvinTemp
-                    let kelvinTemp = weatherData.list[0].main.temp;
-
-                    // Convert Kelvin to Fahrenheit
-                    let fahrenheitTemp = (kelvinTemp - 273.15) * 9/5 + 32;
-      
-                    // Update HTML elements with weather information
-                    document.getElementById('cityName').innerHTML = weatherData.city.name;
-
-
-                    // Set the text of an HTML element with the id "temperature" to the converted temperature
-                    document.getElementById('temperature').innerHTML = "Temp: " + fahrenheitTemp.toFixed(2) + "°F";
-
-                    //document.getElementById('temperature').innerHTML = "Temp: " + weatherData.list[0].main.temp;
-                    document.getElementById('wind').innerHTML = "Wind Speed: " + weatherData.list[0].wind.speed + "mph";
-                    document.getElementById('humidity').innerHTML = "Humidity: " + weatherData.list[0].main.humidity + "%";
-                })
-                .catch(error => {
-                  console.log('Error:', error);
-                });
+                // Loop through the forecast data for the next 5 days
+                for (let i = 0; i < 5; i++) {
+                    const forecastData = fiveDayWeatherData.list[i * 8]; // Update the index to skip 8 data points for each day
+                    const date = forecastData.dt_txt;
+                    const icon = forecastData.weather[0].icon;
+                    const kelvinTemperature = forecastData.main.temp;
+                    const fahrenheitTemperature = (kelvinTemperature - 273.15) * 9/5 + 32;
+                    const windSpeed = forecastData.wind.speed;
+                    const humidity = forecastData.main.humidity;
+                  
+                    // Create a new div element for each day's forecast
+                    const forecastElement = document.createElement('div');
+                    forecastElement.innerHTML = `
+                      <p>${date}</p>
+                      <img src="https://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">
+                      <p>Temperature: ${fahrenheitTemperature} °F</p>
+                      <p>Wind Speed: ${windSpeed} mph</p>
+                      <p>Humidity: ${humidity} %</p>
+                    `;
+                  
+                    // Append the forecast element to the existing forecastElement div
+                    document.getElementById('forecastElement').appendChild(forecastElement);
+                  }
             })
             .catch(error => {
-              console.log('Error:', error);
+                console.log('Error:', error);
             });
-      
-          input.value = '';
-        }
-      };
+        });
+            input.value = '';
 
-    // Add event listener to the form submit button
+            // Save the search history to localStorage
+            const searchHistoryString = localStorage.getItem('searchHistory');
+            const searchHistory = searchHistoryString ? JSON.parse(searchHistoryString) : [];
+            searchHistory.push(cityName);
+            localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+        }
+    };
+
     form.addEventListener('submit', handleFormSubmit);
 
     // Save search history to localStorage
@@ -76,11 +107,68 @@ document.addEventListener('DOMContentLoaded', function() {
         // Get the search history element
         const searchHistoryElement = document.getElementById('searchHistory');
 
+        // Clear the search history element
+        searchHistoryElement.innerHTML = '';
+
         // Loop through the search history array and create list items
         searchHistory.forEach(cityName => {
             const listItem = document.createElement('li');
-            listItem.textContent = cityName;
+            const link = document.createElement('a');
+            link.textContent = cityName;
+            link.href = `/search-results.html?q=${cityName}&format=`;
+            link.addEventListener('click', handleCityClick); // Add event listener to the link
+            listItem.appendChild(link);
             searchHistoryElement.appendChild(listItem);
         });
-    };
+    }
+
+    // Function to handle city click event
+    function handleCityClick(event) {
+        event.preventDefault(); // Prevent default link behavior
+        const cityName = event.target.textContent; // Get the clicked city name
+        
+        // Call a function to search the current and future weather data for the clicked city
+        searchWeather(cityName);
+    }
+    
+    // Function to search the current and future weather data for a city
+    function searchWeather(cityName) {
+        const geoApiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
+    
+        fetch(geoApiUrl)
+        .then(response => response.json())
+        .then(geoData => {
+            const { lat, lon } = geoData[0];
+            
+            const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+            const fiveDayWeatherApi = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+            
+            fetch(weatherApiUrl)
+            .then(response => response.json())
+            .then(weatherData => {
+                console.log(weatherData);
+                
+                // Update the cityData array
+                cityData.push(weatherData);
+                
+                // Retrieve the temperature in Kelvin
+                let kelvinTemp = weatherData.main.temp;
+                
+                // Convert Kelvin to Fahrenheit
+                let fahrenheitTemp = (kelvinTemp - 273.15) * 9/5 + 32;
+                
+                // Update HTML elements with weather information
+                document.getElementById('cityName').innerHTML = weatherData.name;
+                document.getElementById('temperature').innerHTML = "Temp: " + fahrenheitTemp.toFixed(2) + " °F";
+                document.getElementById('wind').innerHTML = "Wind Speed: " + weatherData.wind.speed + " mph";
+                document.getElementById('humidity').innerHTML = "Humidity: " + weatherData.main.humidity + " %";
+            })
+            .catch(error => {
+                console.log('Error:', error);
+            });
+        })
+        .catch(error => {
+            console.log('Error:', error);
+        });
+    }
 });
